2f449bcae621fe9fd773d0884070a273084bfb8c
kim-eun-ji <qwert101311@gmail.com>
2022-03-04 22:58:19 +0900

    feature: 이미지 모듈 개발 완료(임시)

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,59 +1,25 @@
 import {
+  Body,
   Controller,
   Get,
   Post,
+  UploadedFile,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ImageService } from './image.service';
 import multerS3 from 'multer-s3';
 import * as AWS from 'aws-sdk';
 import 'dotenv/config';
-
-const s3 = new AWS.S3();
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post()
-  @UseInterceptors(
-    FilesInterceptor('images', 3, {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET_NAME,
-        acl: 'public-read',
-        // ContentType: 'image/jpeg',
-        ContentType: multerS3.AUTO_CONTENT_TYPE,
-        key: function (req, file, cb) {
-          let newFileName =
-            new Date().valueOf() + Math.random().toString(36).substr(2, 11);
-          let mimeType;
-          switch (file.mimetype) {
-            case 'image/jpeg':
-              mimeType = 'jpg';
-              break;
-            case 'image/png':
-              mimeType = 'png';
-              break;
-            case 'image/gif':
-              mimeType = 'gif';
-              break;
-            case 'image/bmp':
-              mimeType = 'bmp';
-              break;
-            default:
-              mimeType = 'jpg';
-              break;
-          }
-
-          cb(null, `${newFileName}.${mimeType}`);
-        },
-      }),
-    }),
-  )
-  async uploadImage(@UploadedFiles() files: Express.Multer.File) {
-    return this.imageService.uploadImage(files);
+  @UseInterceptors(FileInterceptor('images'))
+  async uploadImage(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    return this.imageService.uploadS3(file);
   }
 }
--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 
@@ -6,5 +7,6 @@ import { ImageService } from './image.service';
   controllers: [ImageController],
   providers: [ImageService],
   exports: [ImageService],
+  imports: [ConfigModule],
 })
 export class ImageModule {}
