4e8b3a7b3b1d84ea51736c561771647a225d2638
kim-eun-ji <qwert101311@gmail.com>
2022-04-17 21:35:24 +0900

    Feature: base response 작성 및 feed swagger 작성(초안)

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -7,21 +7,29 @@ import {
   Param,
   Delete,
   UseInterceptors,
-  UploadedFile,
-  Logger,
   UploadedFiles,
 } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { CreateFeedDto } from './dto/feed.dto';
 import { UpdateFeedDto } from './dto/feed.dto';
 import {
-  ApiOkResponse,
+  ApiCreatedResponse,
   ApiOperation,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { FeedCreateResponse } from 'src/shared/response_entities/feed/temp.response';
 
+// TODO: 400,401,403,404등 공통 사용 응답코드는 컨트롤러에 붙이기
+// @ApiBadRequestResponse({
+//   description: '필수 인자가 없습니다.',
+//   type: HttpError4xxDto,
+// })
+// @ApiUnauthorizedResponse({ description: '인증실패', type: HttpError4xxDto })
+// @ApiForbiddenResponse({ description: '권한(인가)부족', type: HttpError4xxDto })
+// @ApiNotFoundResponse({ description: '해당 리소스 없음', type: HttpError4xxDto })
+// @ApiBearerAuth('accesskey')
 @ApiTags('feed')
 @Controller('feed')
 export class FeedController {
@@ -29,20 +37,23 @@ export class FeedController {
 
   @Post()
   @ApiOperation({ summary: '피드 등록' })
-  @ApiOkResponse({
+  @ApiCreatedResponse({
     description: '성공',
-    type: CreateFeedDto,
-  })
-  @ApiResponse({
-    status: 500,
-    description: '서버 에러',
+    type: FeedCreateResponse,
   })
   @UseInterceptors(FilesInterceptor('images'))
   async create(
     @UploadedFiles() files: Express.Multer.File[],
     @Body() body: CreateFeedDto,
   ) {
-    let result = this.feedService.create(files, body);
+    let status = await this.feedService.create(files, body);
+    let result: FeedCreateResponse = {
+      code: status ? 201 : 500,
+      message: status ? '생성 성공' : '생성 실패',
+      data: {
+        status: status,
+      },
+    };
     return result;
   }
 
