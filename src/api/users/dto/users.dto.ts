d45d06205312d552ff4c348ca9116a0ee4b8f9cf
Jerry <yg_choi@caredoc.kr>
2023-01-22 17:33:57 +0900

    유저 상세조회 API 추가

--- src/api/users/dto/users.dto.ts
+++ src/api/users/dto/users.dto.ts
@@ -64,3 +64,29 @@ export class GetCheckDuplicateUserNicknameReqDTO {
     this.nickname = nickname;
   }
 }
+
+export class GetUserResDTO {
+  @ApiProperty({type: String, example: 'jerry', description: '닉네임'})
+  private nickname: string;
+
+  @ApiProperty({enum: SOCIAL_TYPE, example: 'KAKAO', description: '소셜 종류'})
+  private socialType: string;
+
+  @ApiProperty({type: String, example: '', description: '회원 프로필 이미지'})
+  private image: string;
+
+  @ApiProperty({type: Date, example: '', description: '회원 가입 날짜'})
+  private regDate: Date;
+
+  constructor(
+    nickname: string,
+    socialType: SOCIAL_TYPE,
+    image: string,
+    regDate: Date,
+  ) {
+    this.nickname = nickname;
+    this.socialType = socialType;
+    this.image = image;
+    this.regDate = regDate;
+  }
+}
